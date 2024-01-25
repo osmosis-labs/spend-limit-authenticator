752d6a2073c16120d80fdf62a7598d3453bdbc4f
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-01-25 17:12:58 +0700

    add price strategy struct

--- contracts/spend-limit/src/spend_limit/mod.rs
+++ contracts/spend-limit/src/spend_limit/mod.rs
@@ -1,7 +1,7 @@
 // TODO: Move core spend limit logic here
 
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::Coin;
+use cosmwasm_std::{Addr, Coin};
 
 // The idea is:
 // - track spend limit overtime
@@ -15,4 +15,18 @@ pub struct SpendLimit {
     pub amount_left: u128,
     pub block_of_last_tx: u64,
     pub number_of_blocks_active: u64,
+}
+
+#[cw_serde]
+pub enum PriceStrategy {
+    /// Using just the amount of the given denom to determine quota.
+    AbsoluteValue,
+
+    /// Using a price oracle contract to determine the price of the coin.
+    /// Spending can be done in any coin, as long as price oracle contract provides the price.
+    ///
+    /// The contract must implement the following query:
+    /// - request:  `{ "get_price" { "denom": "<denom>" } }`
+    /// - response: `{ "price": "<price>" }`
+    PriceOracle { contract_address: Addr },
 }
