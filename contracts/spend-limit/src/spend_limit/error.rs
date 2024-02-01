f0074490f8a1a6056526adfdcc9d5078e0963bb3
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-02-01 10:01:32 +0700

    add more tests for has_entered_new_period and fix some bugs

--- contracts/spend-limit/src/spend_limit/error.rs
+++ contracts/spend-limit/src/spend_limit/error.rs
@@ -1,8 +1,12 @@
+use super::period::PeriodError;
 use cosmwasm_std::{Timestamp, Uint128};
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum SpendLimitError {
+    #[error("Period error: {0}")]
+    PeriodError(#[from] PeriodError),
+
     #[error("Overspent: remaining {remaining}, requested {requested}, limit reset timestamp {limit_reset_timestamp}")]
     OverSpent {
         remaining: Uint128,
--- contracts/spend-limit/src/spend_limit/period.rs
+++ contracts/spend-limit/src/spend_limit/period.rs
@@ -1,6 +1,10 @@
 use cosmwasm_schema::cw_serde;
-use time::PrimitiveDateTime;
+use cosmwasm_std::{ensure, Timestamp};
+use thiserror::Error;
+use time::OffsetDateTime;
 
+/// Period of time for spend limit.
+/// Note that week is Monday-based.
 #[cw_serde]
 pub enum Period {
     Day,
@@ -9,29 +13,72 @@ pub enum Period {
     Year,
 }
 
-// TODO: consider error when current < prev_spent_at
-fn is_period_over(
-    period: Period,
-    current: impl Into<PrimitiveDateTime>,
-    prev_spent_at: impl Into<PrimitiveDateTime>,
-) -> bool {
-    let current = current.into();
-    let prev_spent_at = prev_spent_at.into();
+#[derive(Error, Debug, PartialEq)]
+pub enum PeriodError {
+    #[error("Timestamp conversion cause component out of range: {0}")]
+    TimestampConversionOutOfRange(time::error::ComponentRange),
 
-    match period {
-        Period::Day => current.date() > prev_spent_at.date(),
+    #[error("Previous time must be before, or the same as current time: previous {previous}, current {current}")]
+    InvalidTimeComparison {
+        previous: OffsetDateTime,
+        current: OffsetDateTime,
+    },
+}
+
+type PeriodResult<T> = Result<T, PeriodError>;
+
+/// Check whether the current time has entered new [`Period`]
+/// compared to the previous time.
+pub fn has_entered_new_period(
+    period: Period,
+    previous: OffsetDateTime,
+    current: OffsetDateTime,
+) -> PeriodResult<bool> {
+    // if the current time is the same as the previous time,
+    // it means that the tx is executed in the same block.
+    // In that case, we can assume that the tx is not executed in the new period.
+    if previous == current {
+        return Ok(false);
+    }
+
+    // ensure previous time is before or the same as  current time
+    ensure!(
+        previous <= current,
+        PeriodError::InvalidTimeComparison { previous, current }
+    );
+
+    // otherwise, check whether the current time has entered new period
+    // based on the period type.
+    Ok(match period {
+        Period::Day => previous.date() < current.date(),
         Period::Week => {
+            let prev_spent_at_week = previous.monday_based_week();
             let current_week = current.monday_based_week();
-            let prev_spent_at_week = prev_spent_at.monday_based_week();
-            current.year() >= prev_spent_at.year() && current_week > prev_spent_at_week
+
+            let has_entered_new_year = previous.year() < current.year();
+            let is_same_year_but_entered_new_week =
+                previous.year() == current.year() && prev_spent_at_week < current_week;
+
+            has_entered_new_year || is_same_year_but_entered_new_week
         }
         Period::Month => {
-            let current_month: u8 = current.month().into();
-            let prev_spent_at_month: u8 = prev_spent_at.month().into();
-            current.year() >= prev_spent_at.year() && current_month > prev_spent_at_month
+            let prev_spent_at_month = previous.month() as u8;
+            let current_month = current.month() as u8;
+
+            let has_entered_new_year = previous.year() < current.year();
+            let is_same_year_but_entered_new_month =
+                previous.year() == current.year() && prev_spent_at_month < current_month;
+
+            has_entered_new_year || is_same_year_but_entered_new_month
         }
-        Period::Year => current.year() > prev_spent_at.year(),
-    }
+        Period::Year => previous.year() < current.year(),
+    })
+}
+
+/// Convert Timestamp to OffsetDateTime.
+pub fn to_offset_datetime(timestamp: &Timestamp) -> PeriodResult<OffsetDateTime> {
+    OffsetDateTime::from_unix_timestamp_nanos(timestamp.nanos() as i128)
+        .map_err(PeriodError::TimestampConversionOutOfRange)
 }
 
 #[cfg(test)]
@@ -42,25 +89,42 @@ mod tests {
     use time::macros::datetime;
 
     #[rstest]
+    // same time
+    #[case(Period::Day, datetime!(2023-01-01 0:00:00 UTC), datetime!(2023-01-01 00:00:00 UTC), Ok(false))]
+    #[case(Period::Day, datetime!(2025-11-01 0:00:00 UTC), datetime!(2025-11-01 00:00:00 UTC), Ok(false))]
     // day
-    #[case(Period::Day, datetime!(2023-04-10 12:01:10), datetime!(2023-01-02 0:01:00), true)]
-    #[case(Period::Day, datetime!(2023-01-02 0:00:00), datetime!(2023-01-01 0:00:00), true)]
-    #[case(Period::Day, datetime!(2023-01-02 0:00:01), datetime!(2023-01-02 0:00:00), false)]
-    #[case(Period::Day, datetime!(2023-01-02 0:01:02), datetime!(2023-01-02 0:01:00), false)]
+    #[case(Period::Day, datetime!(2023-01-02 0:01:00 UTC), datetime!(2023-04-10 12:01:10 UTC), Ok(true))]
+    #[case(Period::Day, datetime!(2023-01-01 0:00:00 UTC), datetime!(2023-01-02 00:00:00 UTC), Ok(true))]
+    #[case(Period::Day, datetime!(2023-01-02 0:00:00 UTC), datetime!(2023-01-02 00:00:01 UTC), Ok(false))]
+    #[case(Period::Day, datetime!(2026-01-02 0:01:00 UTC), datetime!(2026-01-02 00:01:02 UTC), Ok(false))]
     // week
-    // TODO: add tests
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2024-01-08 00:00:00 UTC), Ok(true))]
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2025-01-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Week, datetime!(2022-03-09 0:05:00 UTC), datetime!(2025-03-14 00:00:00 UTC), Ok(true))]
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2024-02-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2025-01-02 00:00:00 UTC), Ok(true))]
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2024-01-07 23:59:59 UTC), Ok(false))]
+    #[case(Period::Week, datetime!(2024-01-01 0:00:00 UTC), datetime!(2024-01-01 00:00:00 UTC), Ok(false))]
     // month
-    // TODO: add tests
+    #[case(Period::Month, datetime!(2022-01-01 0:00:00 UTC), datetime!(2022-02-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Month, datetime!(2022-01-02 0:01:00 UTC), datetime!(2022-02-02 00:01:02 UTC), Ok(true))]
+    #[case(Period::Month, datetime!(2022-01-01 0:00:00 UTC), datetime!(2022-02-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Month, datetime!(2022-01-01 0:00:00 UTC), datetime!(2023-01-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Month, datetime!(2022-01-01 0:00:00 UTC), datetime!(2022-01-31 23:59:59 UTC), Ok(false))]
     // year
-    #[case(Period::Year, datetime!(2023-01-01 0:00:00), datetime!(2022-01-01 0:00:00), true)]
-    #[case(Period::Year, datetime!(2023-01-02 0:01:02), datetime!(2022-01-02 0:01:00), true)]
-    #[case(Period::Year, datetime!(2022-12-31 23:59:59), datetime!(2022-01-02 0:01:00), false)]
-    fn test_is_period_over(
+    #[case(Period::Year, datetime!(2022-01-01 0:00:00 UTC), datetime!(2023-01-01 00:00:00 UTC), Ok(true))]
+    #[case(Period::Year, datetime!(2022-01-02 0:01:00 UTC), datetime!(2023-01-02 00:01:02 UTC), Ok(true))]
+    #[case(Period::Year, datetime!(2022-01-02 0:01:00 UTC), datetime!(2022-12-31 23:59:59 UTC), Ok(false))]
+    // current < previous
+    #[case(Period::Day, datetime!(2022-01-01 0:00:00 UTC), datetime!(2021-12-31 23:59:59 UTC), Err(PeriodError::InvalidTimeComparison { previous, current }))]
+    #[case(Period::Day, datetime!(2022-05-07 0:00:00 UTC), datetime!(2022-05-06 23:59:59 UTC), Err(PeriodError::InvalidTimeComparison { previous, current }))]
+
+    fn test_has_entered_new_period(
         #[case] period: Period,
-        #[case] current: PrimitiveDateTime,
-        #[case] prev_spent_at: PrimitiveDateTime,
-        #[case] expected: bool,
+        #[case] previous: OffsetDateTime,
+        #[case] current: OffsetDateTime,
+        #[case] expected: PeriodResult<bool>,
     ) {
-        assert_eq!(is_period_over(period, current, prev_spent_at), expected);
+        assert_eq!(has_entered_new_period(period, previous, current), expected);
     }
 }
