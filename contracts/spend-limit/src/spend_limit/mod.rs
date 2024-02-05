5697c9e513ced7b0edff841fe527e602abdc7e93
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-02-05 12:18:11 +0700

    extract spending into it's own module

--- contracts/spend-limit/src/spend_limit/mod.rs
+++ contracts/spend-limit/src/spend_limit/mod.rs
@@ -4,10 +4,10 @@ mod price;
 mod spending;
 
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{ensure, Coin, Fraction};
-use error::SpendLimitError;
+use cosmwasm_std::Coin;
 use period::Period;
 use price::ValueStrategy;
+use spending::Spending;
 
 #[cw_serde]
 pub struct DeprecatedSpendLimit {
@@ -21,10 +21,8 @@ pub struct DeprecatedSpendLimit {
 
 // -------------------------------------------
 
-use cosmwasm_std::{Addr, Timestamp, Uint128};
+use cosmwasm_std::Addr;
 use cw_storage_plus::Map;
-
-use self::{error::SpendLimitResult, period::to_offset_datetime};
 
 pub type SpendingStorage<'a> = Map<'a, SpendingKey<'a>, Spending>;
 
@@ -51,19 +49,6 @@ impl<'a> SpendingKey<'a> {
     }
 }
 
-/// State for tracking spend limit.
-#[cw_serde]
-#[derive(Default)]
-pub struct Spending {
-    /// The value spent in the current period
-    /// This is reset when the period changes
-    pub value_spent_in_period: Uint128,
-
-    /// The last time the account spent
-    /// This is used to check if we are in a new period
-    pub last_spent_at: Timestamp,
-}
-
 #[cw_serde]
 pub struct SpendLimitParams {
     /// Subkey for the account, to allow multiple spend limits per account
@@ -78,180 +63,4 @@ pub struct SpendLimitParams {
 
     /// Strategy to calculate the value of the coin spent.
     value_strategy: ValueStrategy,
-}
-
-impl Spending {
-    pub fn new(last_spent: Timestamp) -> Self {
-        Self {
-            value_spent_in_period: Uint128::zero(),
-            last_spent_at: last_spent,
-        }
-    }
-
-    pub fn spend(
-        self,
-        amount: Uint128,
-        conversion_rate: impl Fraction<Uint128>,
-        limit: Uint128,
-        period: &Period,
-        at: Timestamp,
-    ) -> SpendLimitResult<Self> {
-        let spending_value =
-            amount.multiply_ratio(conversion_rate.numerator(), conversion_rate.denominator());
-
-        let value_spent_in_period = self
-            .get_or_reset_value_spent(period, at)?
-            .checked_add(spending_value)?;
-
-        // ensure that the value spent in the period is not over the limit
-        ensure!(
-            value_spent_in_period <= limit,
-            SpendLimitError::Overspent {
-                remaining: limit.saturating_sub(value_spent_in_period),
-                requested: spending_value,
-            }
-        );
-
-        Ok(Self {
-            value_spent_in_period,
-            last_spent_at: at,
-            ..self
-        })
-    }
-
-    /// Get the value spent in the period.
-    /// If the period has changed, the value spent in the period is reset to zero.
-    fn get_or_reset_value_spent(
-        &self,
-        period: &Period,
-        at: Timestamp,
-    ) -> SpendLimitResult<Uint128> {
-        let previous = to_offset_datetime(&self.last_spent_at)?;
-        let current = to_offset_datetime(&at)?;
-
-        if period.has_changed(previous, current)? {
-            Ok(Uint128::zero())
-        } else {
-            Ok(self.value_spent_in_period)
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use cosmwasm_std::Decimal;
-    use time::macros::datetime;
-    use time::OffsetDateTime;
-
-    use super::*;
-
-    #[test]
-    fn test_spending_flow() {
-        // create new spending tracker
-        let spending = Spending::default();
-        let period = Period::Day;
-
-        assert_eq!(spending.value_spent_in_period, Uint128::zero());
-        assert_eq!(spending.last_spent_at, Timestamp::from_nanos(0));
-
-        // try spending half the limit
-        let limit = Uint128::from(100_000_000u128);
-        let at = to_timestamp(datetime!(2024-01-01 00:00:00 UTC));
-        let conversion_rate = Decimal::one();
-
-        let spending = spending
-            .spend(
-                Uint128::from(50_000_000u128),
-                conversion_rate,
-                limit,
-                &period,
-                at,
-            )
-            .unwrap();
-
-        assert_eq!(
-            spending.value_spent_in_period,
-            Uint128::from(50_000_000u128)
-        );
-        assert_eq!(spending.last_spent_at, at);
-
-        // try spending a bit over the limit
-        let at = to_timestamp(datetime!(2024-01-01 23:59:59 UTC));
-        let err = spending
-            .clone()
-            .spend(
-                Uint128::from(50_000_001u128),
-                conversion_rate,
-                limit,
-                &period,
-                at,
-            )
-            .unwrap_err();
-
-        assert_eq!(
-            err,
-            SpendLimitError::Overspent {
-                remaining: Uint128::zero(),
-                requested: Uint128::from(50_000_001u128),
-            }
-        );
-
-        // try spending a all the limit
-        let at = to_timestamp(datetime!(2024-01-01 23:59:59 UTC));
-        let spending = spending
-            .spend(
-                Uint128::from(50_000_000u128),
-                conversion_rate,
-                limit,
-                &period,
-                at,
-            )
-            .unwrap();
-
-        assert_eq!(
-            spending.value_spent_in_period,
-            Uint128::from(100_000_000u128)
-        );
-        assert_eq!(spending.last_spent_at, at);
-
-        // reset if new period
-        let at = to_timestamp(datetime!(2024-01-02 00:00:00 UTC));
-        let spending = spending
-            .spend(Uint128::zero(), conversion_rate, limit, &period, at)
-            .unwrap();
-
-        assert_eq!(spending.value_spent_in_period, Uint128::zero());
-        assert_eq!(spending.last_spent_at, at);
-    }
-
-    #[test]
-    fn test_spending_with_value_conversion() {
-        let spending = Spending::default();
-        let conversion_rate = Decimal::from_ratio(1u128, 200_000u128);
-        let period = Period::Month;
-
-        // try spending half the limit
-        let limit = Uint128::from(100_000_000u128);
-        let at = to_timestamp(datetime!(2024-01-01 00:00:00 UTC));
-
-        let spending = spending
-            .spend(
-                Uint128::from(50_000_000u128 * 200_000u128),
-                conversion_rate,
-                limit,
-                &period,
-                at,
-            )
-            .unwrap();
-
-        assert_eq!(
-            spending.value_spent_in_period,
-            Uint128::from(50_000_000u128)
-        );
-        assert_eq!(spending.last_spent_at, at);
-    }
-
-    fn to_timestamp(offset_datetime: OffsetDateTime) -> Timestamp {
-        Timestamp::from_nanos(offset_datetime.unix_timestamp_nanos() as u64)
-    }
 }
--- contracts/spend-limit/src/spend_limit/spending.rs
+++ contracts/spend-limit/src/spend_limit/spending.rs
@@ -1,13 +1,88 @@
 use std::collections::HashMap;
 
-use cosmwasm_std::{Coin, Uint128};
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{ensure, Coin, Fraction, Timestamp, Uint128};
+
+use crate::spend_limit::error::SpendLimitError;
+
+use super::{
+    error::SpendLimitResult,
+    period::{to_offset_datetime, Period},
+};
+
+/// State for tracking spend limit.
+#[cw_serde]
+#[derive(Default)]
+pub struct Spending {
+    /// The value spent in the current period
+    /// This is reset when the period changes
+    pub value_spent_in_period: Uint128,
+
+    /// The last time the account spent
+    /// This is used to check if we are in a new period
+    pub last_spent_at: Timestamp,
+}
+
+impl Spending {
+    pub fn new(last_spent: Timestamp) -> Self {
+        Self {
+            value_spent_in_period: Uint128::zero(),
+            last_spent_at: last_spent,
+        }
+    }
+
+    pub fn spend(
+        self,
+        amount: Uint128,
+        conversion_rate: impl Fraction<Uint128>,
+        limit: Uint128,
+        period: &Period,
+        at: Timestamp,
+    ) -> SpendLimitResult<Self> {
+        let spending_value =
+            amount.multiply_ratio(conversion_rate.numerator(), conversion_rate.denominator());
+
+        let value_spent_in_period = self
+            .get_or_reset_value_spent(period, at)?
+            .checked_add(spending_value)?;
+
+        // ensure that the value spent in the period is not over the limit
+        ensure!(
+            value_spent_in_period <= limit,
+            SpendLimitError::Overspent {
+                remaining: limit.saturating_sub(value_spent_in_period),
+                requested: spending_value,
+            }
+        );
+
+        Ok(Self {
+            value_spent_in_period,
+            last_spent_at: at,
+            ..self
+        })
+    }
+
+    /// Get the value spent in the period.
+    /// If the period has changed, the value spent in the period is reset to zero.
+    fn get_or_reset_value_spent(
+        &self,
+        period: &Period,
+        at: Timestamp,
+    ) -> SpendLimitResult<Uint128> {
+        let previous = to_offset_datetime(&self.last_spent_at)?;
+        let current = to_offset_datetime(&at)?;
+
+        if period.has_changed(previous, current)? {
+            Ok(Uint128::zero())
+        } else {
+            Ok(self.value_spent_in_period)
+        }
+    }
+}
 
 /// Calculate the spendings from the pre-execution balances and the post-execution balances.
 /// Ignores received coins.
-pub fn calculate_spendings(
-    balances_pre_exec: Vec<Coin>,
-    balances_post_exec: Vec<Coin>,
-) -> Vec<Coin> {
+pub fn spent_coins(balances_pre_exec: Vec<Coin>, balances_post_exec: Vec<Coin>) -> Vec<Coin> {
     let balances_post_exec = to_balances_map(balances_post_exec);
 
     // Goes through all pre-execution balances and checks if they were spent.
@@ -53,6 +128,120 @@ fn to_balances_map(balances: Vec<Coin>) -> HashMap<String, Uint128> {
 mod tests {
     use super::*;
     use rstest::rstest;
+
+    use cosmwasm_std::Decimal;
+    use time::macros::datetime;
+    use time::OffsetDateTime;
+
+    #[test]
+    fn test_spending_flow() {
+        // create new spending tracker
+        let spending = Spending::default();
+        let period = Period::Day;
+
+        assert_eq!(spending.value_spent_in_period, Uint128::zero());
+        assert_eq!(spending.last_spent_at, Timestamp::from_nanos(0));
+
+        // try spending half the limit
+        let limit = Uint128::from(100_000_000u128);
+        let at = to_timestamp(datetime!(2024-01-01 00:00:00 UTC));
+        let conversion_rate = Decimal::one();
+
+        let spending = spending
+            .spend(
+                Uint128::from(50_000_000u128),
+                conversion_rate,
+                limit,
+                &period,
+                at,
+            )
+            .unwrap();
+
+        assert_eq!(
+            spending.value_spent_in_period,
+            Uint128::from(50_000_000u128)
+        );
+        assert_eq!(spending.last_spent_at, at);
+
+        // try spending a bit over the limit
+        let at = to_timestamp(datetime!(2024-01-01 23:59:59 UTC));
+        let err = spending
+            .clone()
+            .spend(
+                Uint128::from(50_000_001u128),
+                conversion_rate,
+                limit,
+                &period,
+                at,
+            )
+            .unwrap_err();
+
+        assert_eq!(
+            err,
+            SpendLimitError::Overspent {
+                remaining: Uint128::zero(),
+                requested: Uint128::from(50_000_001u128),
+            }
+        );
+
+        // try spending a all the limit
+        let at = to_timestamp(datetime!(2024-01-01 23:59:59 UTC));
+        let spending = spending
+            .spend(
+                Uint128::from(50_000_000u128),
+                conversion_rate,
+                limit,
+                &period,
+                at,
+            )
+            .unwrap();
+
+        assert_eq!(
+            spending.value_spent_in_period,
+            Uint128::from(100_000_000u128)
+        );
+        assert_eq!(spending.last_spent_at, at);
+
+        // reset if new period
+        let at = to_timestamp(datetime!(2024-01-02 00:00:00 UTC));
+        let spending = spending
+            .spend(Uint128::zero(), conversion_rate, limit, &period, at)
+            .unwrap();
+
+        assert_eq!(spending.value_spent_in_period, Uint128::zero());
+        assert_eq!(spending.last_spent_at, at);
+    }
+
+    #[test]
+    fn test_spending_with_value_conversion() {
+        let spending = Spending::default();
+        let conversion_rate = Decimal::from_ratio(1u128, 200_000u128);
+        let period = Period::Month;
+
+        // try spending half the limit
+        let limit = Uint128::from(100_000_000u128);
+        let at = to_timestamp(datetime!(2024-01-01 00:00:00 UTC));
+
+        let spending = spending
+            .spend(
+                Uint128::from(50_000_000u128 * 200_000u128),
+                conversion_rate,
+                limit,
+                &period,
+                at,
+            )
+            .unwrap();
+
+        assert_eq!(
+            spending.value_spent_in_period,
+            Uint128::from(50_000_000u128)
+        );
+        assert_eq!(spending.last_spent_at, at);
+    }
+
+    fn to_timestamp(offset_datetime: OffsetDateTime) -> Timestamp {
+        Timestamp::from_nanos(offset_datetime.unix_timestamp_nanos() as u64)
+    }
 
     #[rstest]
     #[case::no_delta(vec![], vec![], vec![])]
@@ -102,7 +291,7 @@ mod tests {
         #[case] balances_after_spent: Vec<Coin>,
         #[case] expected: Vec<Coin>,
     ) {
-        let deltas = calculate_spendings(balances_before_spent, balances_after_spent);
+        let deltas = spent_coins(balances_before_spent, balances_after_spent);
         assert_eq!(expected, deltas);
     }
 }
