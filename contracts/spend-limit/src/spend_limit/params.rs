12aba2568a7ce38f36e5a66c8f26db317bbf573e
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-02-19 11:30:52 +0700

    use fixed test-tube's authenticator branch instead of local

--- contracts/spend-limit/src/spend_limit/params.rs
+++ contracts/spend-limit/src/spend_limit/params.rs
@@ -6,6 +6,7 @@ use super::period::Period;
 #[cw_serde]
 pub struct SpendLimitParams {
     /// Subkey for the account, to allow multiple spend limits per account
+    /// TODO: After authenticator id is passed in as a request, we can remove this field
     pub subkey: String,
 
     /// Limit per period, used to enforce spend limit with this given amount and denom.
